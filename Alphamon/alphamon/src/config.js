43674810ab31fdadf7e03c2b469a6472ec9d3832
yeon121 <ba12212@naver.com>
2023-10-04 17:52:14 +0900

    gitignore_설정변경

---
+++ Alphamon/alphamon/src/config.js
@@ -0,0 +1,13 @@
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+const loginEndpoint = process.env.REACT_APP_ENDPOINT_LOGIN;
+const joinEndpoint = process.env.REACT_APP_ENDPOINT_JOIN;
+const sendEndpoint = process.env.REACT_APP_ENDPOINT_CHAT_SEND;
+
+module.exports = {
+  serverUrl,
+  loginEndpoint,
+  joinEndpoint, 
+  sendEndpoint
+};
+
+//회원가입 적용해놓았으니 그거 참고하시면 될 것 같습니당 ~
